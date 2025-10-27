54cd5d119dfafc299c26c7613c06c27164825a9f
Chris Hillman <kt-five@hotmail.com>
2025-10-27 04:11:33 +0100

    retry

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  // url: 'https://the-kt-app.onrender.com',
+  url: 'https://the-kt-app.onrender.com',
   petImagesUrl: 'https://res.cloudinary.com/daxrovkug/image/upload/v1746460136/ktApp-petMainPic/',
-  url: 'http://localhost:3001'
+  // url: 'http://localhost:3001'
 };
 
 /*
